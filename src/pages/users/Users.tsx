a9de7b113e0082e6d6662abd59f1f03313cd2e95
Andrii Pravnyk <apravnyk@bigid.com>
2023-03-30 22:34:54 +0300

    Fixes

--- src/pages/users/Users.tsx
+++
@@ -1,9 +0,0 @@
-import { StyledUsers } from '@/pages/users/UsersStyles';
-
-export default function Users() {
-  return (
-    <StyledUsers.Container>
-      <StyledUsers.List>Users!</StyledUsers.List>
-    </StyledUsers.Container>
-  );
-}
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,1 +1,20 @@
-export { default } from './Users';
+import { styled } from '@mui/material/styles';
+
+const StyledUsers = {
+  Container: styled('div')({
+    display: 'flex',
+    alignItems: 'start',
+    width: '100%',
+  }),
+  List: styled('div')({
+    display: 'flex',
+  }),
+};
+
+export default function Users() {
+  return (
+    <StyledUsers.Container>
+      <StyledUsers.List>Users</StyledUsers.List>
+    </StyledUsers.Container>
+  );
+}
