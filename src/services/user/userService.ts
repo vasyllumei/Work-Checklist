e75c8cd8d4fbe5c315c58f63ed3199a7452909ba
Andrii Pravnyk <andrii.pravnyk@Blackthorn-vision.com>
2023-04-01 19:26:53 +0300

    minor fixes

--- src/services/user/userService.ts
+++ src/services/user/userService.ts
@@ -1,1 +1,5 @@
-import { UserType } from '@/types/User';import { api, ResponseType } from '@/services/apiService';export const getUsers = async (): Promise<ResponseType<UserType[]>> =>  await api.get<ResponseType<UserType[]>>('/users').then(resolve => resolve.data);
+import { UserType } from '@/types/User';
+import { api, ResponseType } from '@/services/apiService';
+
+export const getUsers = async (): Promise<ResponseType<UserType[]>> =>
+  await api.get<ResponseType<UserType[]>>('/users').then(resolve => resolve.data);
