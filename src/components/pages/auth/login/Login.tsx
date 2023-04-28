c7aca7e61d4db528279e906f0a94b55eed2763e0
VasylLum <vlumewot3@gmail.com>
2023-04-27 22:19:52 -0400

    dont delete, thank you

--- src/components/pages/auth/login/Login.tsx
+++ src/components/pages/auth/login/Login.tsx
@@ -1,5 +1,4 @@
-import { AuthBaseTemplate } from '@/components/pages/auth/components/AuthBaseTemplate';
-import { Stack, Typography } from '@mui/material';
+
 import { FC, useState } from 'react';
 import { TextInput } from '@/components/TextInput';
 import { Button } from '@/components/Button';
@@ -20,46 +19,54 @@ export const Login: FC = () => {
         router.push('/');
     };
     return (
-        <AuthBaseTemplate headTitle="Portal | Login">
-            <form className={styles.loginForm}>
-                <Stack justifyContent="start">
-                    <Typography variant="h4" className={styles.headText}>
-                        Sign In
-                    </Typography>
-                    <Typography variant="subtitle1" className={styles.frontText}>
-                        Enter your email and password to sign in!
-                    </Typography>
-                    <div className={styles.lineGroup}>
-                        <hr className={styles.line} />
-                        <div className={styles.or}>or</div>
-                        <hr className={styles.line} />
-                    </div>
-                    <label htmlFor="uname">
-                        <b className={styles.inputEmail}>Email*</b>
-                        <TextInput value={email} onChange={setEmail} placeHolder="mail@simmmple.com" />
-                    </label>
-                    <label htmlFor="uname">
-                        <b className={styles.inputPassword}>Password*</b>
-                        <TextInput value={password} onChange={setPassword} placeHolder={'Min. 8 characters'} />
-                    </label>
-                    <div>
-                        <label>
-                            <input
-                                type="checkbox"
-                                checked={rememberMe}
-                                onChange={handleRememberMe}
-                                name="remember"
-                                className={styles.checkbox}
-                            />{' '}
-                            Keep me logged in
-                        </label>
-                    </div>
-                    <Button text={'Sign In'} onClick={login} />
-                    <div className={styles.forgotText}>
-                        Not registered yet? <a href="/signup">Create an Account</a>
-                    </div>
-                </Stack>
+
+        <body className={styles.body}>
+        <div className={styles.left} id="left">
+            <form className={styles.form}>
+                <div className={styles.headText}> Sign In </div>
+                <div className={styles.frontText}>Enter your email and password to sign in!</div>
+            <div className={styles.lineGroup}>
+                <hr className={styles.line}/>
+                <div className={styles.or}>or</div>
+                <hr className={styles.line}/>
+            </div>
+            <label htmlFor="uname">
+                <b className={styles.inputEmail}>Email*</b>
+                <TextInput value={email} onChange={setEmail} placeHolder="mail@simmmple.com"/>
+            </label>
+            <label htmlFor="uname">
+                <b className={styles.inputPassword}>Password*</b>
+                <TextInput value={password} onChange={setPassword} placeHolder={'Min. 8 characters'}/>
+            </label>
+            <div className={styles.checkboxContainer}>
+                <label>
+                    <input
+                        type="checkbox"
+                        checked={rememberMe}
+                        onChange={handleRememberMe}
+                        name="remember"
+                    />
+                    Keep me logged in
+                </label>
+                <a href="/signup">Forget password?</a>
+            </div>
+            <Button text={'Sign In'} onClick={login}/>
+            <div className={styles.forgotText}>
+                Not registered yet? <a href="/signup">Create an Account</a>
+            </div>
             </form>
-        </AuthBaseTemplate>
+
+            <footer className={styles.footer}>
+                © 2022 Horizon UI. All Rights Reserved. Made with love by Simmmple!</footer>
+            <div className={styles.right} id="right">
+                <div className={styles.square}>
+                  <div className={styles.logo}>
+                <img src="https://i.ibb.co/Tq5b2hy/Screenshot-2.png" alt="Логотип"/>
+                  </div>
+                <div/>
+            </div>
+            </div>
+        </div>
+        </body>
     );
 };
