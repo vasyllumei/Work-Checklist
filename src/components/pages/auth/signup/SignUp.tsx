cc2fe4f5b4ec528c0d1723bde0ed7994d7309d15
VasylLum <vlumewot3@gmail.com>
2023-05-07 14:09:41 -0400

    Change styles

--- src/components/pages/auth/signup/SignUp.tsx
+++ src/components/pages/auth/signup/SignUp.tsx
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { FC, useState } from 'react';
+import { TextInput } from '@/components/TextInput';
+import { useRouter } from 'next/router';
+import styles from './SignUp.module.css';
 
-export const Register = () => {
+export const SignUp: FC = () => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const router = useRouter();
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -13,33 +17,32 @@ export const Register = () => {
       alert('Passwords do not match');
       return;
     }
-    // handle form submission
+    router.push('/success');
   };
 
   return (
-    <div>
+    <div className={styles.mainContainer}>
       <form onSubmit={handleSubmit}>
         <label>
           First Name:
-          <input type="text" value={firstName} onChange={e => setFirstName(e.target.value)} />
+          <TextInput value={firstName} onChange={setFirstName} placeHolder={'Min. 8 characters'} />
         </label>
         <label>
           Last Name:
-          <input type="text" value={lastName} onChange={e => setLastName(e.target.value)} />
+          <TextInput value={lastName} onChange={setLastName} placeHolder={'Min. 8 characters'} />
         </label>
         <label>
           Email:
-          <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+          <TextInput value={email} onChange={setEmail} placeHolder={'Enter your email address'} />
         </label>
         <label>
           Password:
-          <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+          <TextInput value={password} onChange={setPassword} placeHolder={'Min. 8 characters'} />
         </label>
         <label>
           Confirm Password:
-          <input type="password" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} />
+          <TextInput value={confirmPassword} onChange={setConfirmPassword} placeHolder={'Min. 8 characters'} />
         </label>
-        <button type="submit">Register</button>
       </form>
     </div>
   );
---
+++ src/pages/signUp.tsx
@@ -0,0 +1,2 @@
+import { SignUp } from '@/components/pages/auth/signup';
+export default SignUp;
